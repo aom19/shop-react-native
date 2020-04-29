9d54d4ca59219678dbca8aae0c6936c8b761ccb0
sub_zero <ostapovici.anton@gmail.com>
2020-04-29 20:55:22 +0300

    Add Screen Product Detail

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import {View, Text ,Image ,StyleSheet ,Button} from 'react-native';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity, Platform } from 'react-native';
 
 import Colors from '../../constants/Color'
 import Color from '../../constants/Color';
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 
-const ProductItem = props =>{
-    return(
-        <View style = {styles.product}> 
-            <View style ={styles.imageContainer}>
-                <Image source={{uri : props.image}} style={styles.image}/>
-            </View>
-            <View style = {styles.details}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.price}> ${props.price.toFixed(2)}</Text>
-            </View>
-            <View style ={styles.actions}>
-                <Button  
-                    color ={Colors.primary}
-                    title ="View Details" 
-                    onPress ={props.onViewDetail}
-                />
-                <Button  
-                    color ={Colors.accent}
-                    title = "Add to cart" 
-                    onPress ={props.onAddToCart}
-                />
+const ProductItem = props => {
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
+
+    return (
+        <View style={styles.product}>
+            <View style = {styles.touchable}>
+            <TouchableCmp onPress={props.onViewDetail} useForeground>
+                <View >
+                <View style={styles.imageContainer}>
+                    <Image source={{ uri: props.image }} style={styles.image} />
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.title}>{props.title}</Text>
+                    <Text style={styles.price}> ${props.price.toFixed(2)}</Text>
+                </View>
+                <View style={styles.actions}>
+                    <Button
+                        color={Colors.primary}
+                        title="View Details"
+                        onPress={props.onViewDetail}
+                    />
+                    <Button
+                        color={Colors.accent}
+                        title="Add to cart"
+                        onPress={props.onAddToCart}
+                    />
+                </View>
+                </View>
+            </TouchableCmp>
             </View>
         </View>
     );
@@ -32,50 +45,54 @@ const ProductItem = props =>{
 
 
 const styles = StyleSheet.create({
-    product:{
-        shadowColor :'black',
-        shadowOpacity : 0.26,
-        shadowOffset: {width: 0 , height :2},
+    product: {
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
         shadowRadius: 8,
         elevation: 5,
-        borderRadius: 10,
-        backgroundColor:'white',
+        borderRadius: 10, 
+        backgroundColor: 'white',
         height: 300,
-        margin:20,
+        margin: 20,
+        
     },
-    imageContainer:{
-        width:'90%',
-        height:'60%',
-        borderTopLeftRadius :10,
-        borderTopRightRadius :10,
-        overflow :"hidden",
-
+    touchable:{
+        overflow:'hidden',
+        borderRadius: 10,
     },
-    image :{
-        width:'100%',
-        height:'100%'
+    imageContainer: {
+        width: '100%',
+        height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: "hidden",
     },
-    title:{
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    title: {
         fontSize: 18,
-        marginVertical:4,
-    
-    },
-    price:{
-        fontSize:14,
-        color:'#888'
-    },
-    actions:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        height:'25%',
-        paddingHorizontal :20 
+        marginVertical: 4,
 
     },
-    details:{
-        alignItems:'center',
-        height:'15%',
-        padding :10
+    price: {
+        fontSize: 14,
+        color: '#888'
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '25%',
+        paddingHorizontal: 20
+
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10
     }
 })
 
