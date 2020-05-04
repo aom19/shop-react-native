a04c9c671cbeef6d2f81c4f4f3a055cffe9a10a4
sub_zero <ostapovici.anton@gmail.com>
2020-05-04 17:30:26 +0300

    Refactoring Product Item and Products screen

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -15,7 +15,7 @@ const ProductItem = props => {
     return (
         <View style={styles.product}>
             <View style = {styles.touchable}>
-            <TouchableCmp onPress={props.onViewDetail} useForeground>
+            <TouchableCmp onPress={props.onSelect} useForeground>
                 <View >
                 <View style={styles.imageContainer}>
                     <Image source={{ uri: props.image }} style={styles.image} />
@@ -25,16 +25,7 @@ const ProductItem = props => {
                     <Text style={styles.price}> ${props.price.toFixed(2)}</Text>
                 </View>
                 <View style={styles.actions}>
-                    <Button
-                        color={Colors.primary}
-                        title="View Details"
-                        onPress={props.onViewDetail}
-                    />
-                    <Button
-                        color={Colors.accent}
-                        title="Add to cart"
-                        onPress={props.onAddToCart}
-                    />
+                   {props.children}
                 </View>
                 </View>
             </TouchableCmp>
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,66 +1,83 @@
 import React from 'react';
-import { FlatList, View, Text, StyleSheet, Platform } from 'react-native'
-import { useSelector , useDispatch } from 'react-redux';
-import { HeaderButtons ,Item} from 'react-navigation-header-buttons'
+import { FlatList, View, Button, Text, StyleSheet, Platform } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
 import HeaderButton from '../../components/UI/HeaderButton'
 import ProductItem from '../../components/shop/ProductItem'
 import * as cartActions from '../../store/actions/cart';
-
+import Colors from '../../constants/Color'
 
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
     const dispatch = useDispatch();
+
+    const selectItemHandler = (id, title) => {
+        props.navigation.navigate('ProductDetail', {
+            productId: id,
+            productTitle: title
+        })
+    }
+
     return (
         <FlatList
             data={products}
             keyExtractor={item => item.id}
             renderItem={itemData => (
-                <ProductItem 
-                image ={itemData.item.imageUrl}
-                title ={itemData.item.title}
-                price ={itemData.item.price}
-                onViewDetail ={() => {
-                    props.navigation.navigate('ProductDetail' , {
-                            productId: itemData.item.id,
-                            productTitle : itemData.item.title
-                        })
-                }}
-                onAddToCart  ={() => {
-                    dispatch(cartActions.addToCart(itemData.item));
-                }}
-                />
+                <ProductItem
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onSelect={() => {
+                        selectItemHandler(itemData.item.id, itemData.item.title);
+                    }} >
+                    <Button
+                        color={Colors.primary}
+                        title="View Details"
+                        onPress={() => {
+                            selectItemHandler(itemData.item.id, itemData.item.title)
+                        }}
+                    />
+                    <Button
+                        color={Colors.accent}
+                        title="Add to cart"
+                        onPress={() => {
+                            dispatch(cartActions.addToCart(itemData.item));
+                        }}
+                    />
+                </ProductItem>
             )}
         />
-      
+
     )
 };
 
 ProductsOverviewScreen.navigationOptions = navData => {
-    return{
+    return {
         headerTitle: 'All Products',
-            headerLeft : () =>(
-                <HeaderButtons HeaderButtonComponent ={HeaderButton}>
-                    <Item 
-                        title= "Menu"
-                        iconName ={Platform.OS === 'android' ? 'md-menu' :'ios-menu'}
-                        onPress ={() =>{
-                            navData.navigation.toggleDrawer();
-                        }}
-                    />
-                </HeaderButtons>
-            ),
-            headerRight : () => (
-                <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item
-                        title='Cart'
-                        iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
-                        onPress={() => { 
-                            navData.navigation.navigate('Cart'); 
-                        }}
-                    />
-                </HeaderButtons>
-            )
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Menu"
+                    iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        ),
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title='Cart'
+                    iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                    onPress={() => {
+                        navData.navigation.navigate('Cart');
+                    }}
+                />
+            </HeaderButtons>
+        )
     }
 };
 
